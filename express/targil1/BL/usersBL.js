1c99864e9764bf0686d015cd51c3a06929506d81
Elad Hamneshin <hamneshin123@gmail.com>
2023-08-20 00:03:03 +0300

    {update}-express targil1 bl and controller finished

--- express/targil1/BL/usersBL.js
+++ express/targil1/BL/usersBL.js
@@ -2,5 +2,56 @@ import bcrypt from "bcrypt";
 import PasswordValidator from "password-validator";
 import emailValidator from "email-validator";
 import { v4 as uuidv4 } from "uuid";
-import * as usersDL from "../DL/usersDL.js";
+import usersDL from "../DL/usersDL.js";
 
+
+const passwordValidator = new PasswordValidator();
+passwordValidator.is().min(8).has().uppercase().has().lowercase();
+
+
+export function getUsers() {
+    return usersDL.getUsers();
+}
+
+export function getUserById(id) {
+    return usersDL.getUserById(id);
+}
+
+export async function isEmailPasswordExist(email, password) {
+    const users = usersDL.getUsersByEmail(email);
+
+    if (users.length === 0) 
+        return false;
+
+    for (const user of users) {
+        const res = await bcrypt.compare(password, user.password);
+        if (res) 
+            return true;
+    }
+
+    return false;
+}
+
+export async function addUser(email, password) {
+    if(!emailValidator.validate(email))
+        throw new Error("email is not valid");
+
+    if(!passwordValidator.validate(password))
+        throw new Error("password is not valid");
+
+    const hash = await bcrypt.hash(password, 10);
+    const user = { id: uuidv4(), email, password: hash };
+    usersDL.addUser(user);
+    return user;
+}
+
+export function deleteUser(id) {
+    usersDL.deleteUser(id);
+}
+
+export async function updateUser(id, email, password) {
+    const user = usersDL.getUserById(id);
+    user.email = email;
+    user.password = await bcrypt.hash(password, 10);
+    usersDL.updateUser(user);
+}
--- express/targil1/contollers/usersController.js
+++ express/targil1/contollers/usersController.js
@@ -1,99 +1,57 @@
-import bcrypt from "bcrypt";
-import PasswordValidator from "password-validator";
-import emailValidator from "email-validator";
-import { v4 as uuidv4 } from "uuid";
-import * as usersDL from "../DL/usersDL.js";
-
-const passwordValidator = new PasswordValidator();
-passwordValidator.is().min(8).has().uppercase().has().lowercase();
-
+import usersBL from "../BL/usersBL.js";
 
 export function getUsers(req, res) {
-    res.send(usersDL.getUsers());
+    res.send(usersBL.getUsers());
 }
-
 
 export function getUserById(req, res) {
     try {
-        const user = usersDL.getUserById(req.params.id);
+        const user = usersBL.getUserById(req.params.id);
         res.send(user);
     } catch (error) {
         res.send(error.message);
     }
 }
 
-export function getUserByEmail(req, res) {
-    const { email, password } = req.body;
-    const users = usersDL.getUsersByEmail(email);
-
-    if (users.length === 0) {
-        res.send(" wrong credentials");
-        return;
-    }
-    for (const user of users) {
-        bcrypt.compare(password, user.password, (err, result) => {
-            if (err) {
-                res.send(err.message);
-                return;
-            }
-
-            if (result) {
-                res.send("User is connected");
-                return;
-            }
-
-            res.send(" wrong credentials");
-        });
-    }
-}
-
-export function addUser(req, res) {
+export async function isEmailPasswordExist(req, res) {
     const { email, password } = req.body;
 
-    if (!emailValidator.validate(email)) {
-        res.send("email is not valid");
-        return;
+    try{
+        const res = await usersBL.isEmailPasswordExist(email, password);
+    } catch(err){
+        res.send(err.message);
     }
 
-    if (!passwordValidator.validate(password)) {
-        res.send("password is not valid");
-        return;
-    }
-
-    bcrypt.hash(password, 10, (err, hash) => {
-        if (err) {
-            res.send(err.message);
-            return;
-        }
-        const user = { id: uuidv4(), email, password: hash };
-        usersDL.addUser(user);
-        res.send(usersDL.getUsers());
-    });
+    res? res.send("user is connected") : res.send("wrong credentials");
 }
 
-function deleteUser(req, res) {
+export async function addUser(req, res) {
+    const { email, password } = req.body;
+
     try {
-        usersDL.deleteUser(req.params.id);
-        res.send(usersDL.getUsers());
+        const user = await usersBL.addUser(email, password);
+        res.send(usersBL.getUsers());
     } catch (error) {
         res.send(error.message);
     }
 }
 
-export function updateUser(req, res) {
-    const { email, password } = req.body;
-    let user;
-
+export function deleteUser(req, res) {
     try {
-        user = usersDL.getUserById(req.params.uid);
+        usersBL.deleteUser(req.params.id);
+        res.send(usersBL.getUsers());
     } catch (error) {
         res.send(error.message);
     }
+}
 
-    bcrypt.hash(password, 10, (err, hash) => {
-        user.password = hash;
-        user.email = email;
-        usersDL.updateUser(user);
-        res.send(usersDL.getUsers());
-    });
+export async function updateUser(req, res) {
+    const { email, password } = req.body;
+    
+    try {
+        await usersBL.updateUser(req.params.id, email, password);
+        res.send(usersBL.getUsers());
+    } catch (error) {
+        res.send(error.message);
+    }
 }
