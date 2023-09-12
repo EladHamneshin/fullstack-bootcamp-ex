c3f58f594919e8725c185ab92e35503c268c1695
Elad Hamneshin <hamneshin123@gmail.com>
2023-09-12 17:13:22 +0300

    {add} sql from js with pj

---
+++ SQL/temp.js
@@ -0,0 +1,25 @@
+const { Client } = require("pg")
+
+
+const config = {
+    user: "postgres",
+    database: "northwind ",
+    host: "localhost",
+    password: "Kodcode2023",
+    port: "5432",
+}
+
+const connectDb = async () => {
+    try {
+        const client = new Client(config)
+        await client.connect()
+        const query = "SELECT product_id,  SUM(quantity) FROM order_details GROUP BY product_id"
+        const res = await client.query(query)
+        console.log(res.rows)
+        await client.end()
+    } catch (error) {
+        console.log(error)
+    }
+}
+ 
+connectDb()
